8867cc43a43520427ffdc47e1bb57597052cee61
berger <robertberger.dev@gmail.com>
2022-12-30 11:59:04 -0500

    appended h3 to factWrapper

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ async function fetchCat() {
     var catFact = await fetch('https://catfact.ninja/fact?max_length=140')
         .then(response => response.json())
         console.log(catFact.fact);
+
+        var catFactTitle = document.createElement('h3');
+        catFactTitle.innerHTML = "Random Cat Fact";
+
+        var factWrapper = document.getElementById('factWrapper');
+        factWrapper.appendChild(catFactTitle);
 }
 
 fetchCat();
