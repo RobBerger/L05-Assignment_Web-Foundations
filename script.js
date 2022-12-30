15ddb4be3e51f75955dd86bb92ab1b6ca015469f
berger <robertberger.dev@gmail.com>
2022-12-30 12:05:02 -0500

    finalized project

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 async function fetchCat() {
     var catFact = await fetch('https://catfact.ninja/fact?max_length=140')
         .then(response => response.json())
-        console.log(catFact.fact);
 
         var catFactTitle = document.createElement('h3');
         catFactTitle.innerHTML = "Random Cat Fact";
 
+        var catFactText = document.createElement('p');
+        catFactText.innerHTML = catFact.fact;
+
         var factWrapper = document.getElementById('factWrapper');
         factWrapper.appendChild(catFactTitle);
+        factWrapper.appendChild(catFactText);
 }
 
 fetchCat();
