bfa62e1a9f6278ab02787d8f01f2b4e083c5163d
berger <robertberger.dev@gmail.com>
2022-12-30 11:37:12 -0500

    converted the responce to json

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 async function fetchCat() {
-    fetch('https://catfact.ninja/fact?max_length=140');
+    var catFact = await fetch('https://catfact.ninja/fact?max_length=140')
+        .then(response => response.json())
+        console.log(catFact);
 }
+
+fetchCat();
